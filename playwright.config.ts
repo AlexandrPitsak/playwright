644e1823c26a7c1ada09f50f2b6fbdc1f1ed6f67
Alexandr Pitsak <alexandr.pitsak@namecheap.com>
2023-11-10 16:26:25 +0200

    add axios

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 // require('dotenv').config();
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e/tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
